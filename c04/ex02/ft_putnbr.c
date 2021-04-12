6785be52136268032b72973a6d13ec0a60432689
Amir <thebluemitsu@gmail.com>
2021-04-12 17:57:04 +0300

    edit c04

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -12,12 +12,12 @@
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+static void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	ft_putstr(char *str)
+static void	ft_putstr(char *str)
 {
 	while (*str)
 	{
